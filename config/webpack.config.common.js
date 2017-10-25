cf355c21f23c8495bca35b335a92e4980362e144
Jay Kan <leicasper@gmail.com>
2017-10-25 14:52:54 -0700

    build: add common webpack config

---
+++ config/webpack.config.common.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 20000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true,
+    }),
+  ],
+};
+

386c30be72736de13cd2e349dca837cb7b969c6f
Jay Kan <leicasper@gmail.com>
2017-10-26 13:48:02 -0700

    build(webpack): add webpack dev config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const config = require('./webpack.config.common');
+const helpers = require('./helpers');
+
+module.exports = merge.smart(config, {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'react-hot-loader/patch',
+    './src/index',
+  ],
+  output: {
+    path: helpers.root('dist'), 
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /^((?!\.module).)*\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ]
+  }
+});
